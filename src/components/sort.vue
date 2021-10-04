a25592aabe9e524f091b0779ede969804b9b2b74
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2021-10-04 18:39:03 +0300

    add buttons

--- src/components/sort.vue
+++ src/components/sort.vue
@@ -2,21 +2,36 @@
   <div>
     <h1>{{ arr }}</h1>
     <h2>{{ sortedarr }}</h2>
+    <div id="buttongen">
+        <button v-on:click="regenerateRandomArray">Generate Array</button>        
+    </div>
+    <div id="buttonsort">
+        <button v-on:click="reMergeSort">Sort Array</button>        
+    </div> 
   </div>
 </template>
 
 <script>
 import mergeSort from '../..//src/mergeSort'
 import generateRandomArray from '../..//src/generateRandomArray'
-let array = generateRandomArray();
+
 export default {
-  name: 'sort', 
-  data() {
+  name: 'sort',
+  data () {
     return {
-        arr : Array.from(array),
-        sortedarr : mergeSort(array)
+        arr:[],
+        sortedarr: []
+        //sortedarr: mergeSort(Array.from(array))
     }
-  },
+}, 
+ methods:{
+    regenerateRandomArray: function () {
+        this.arr = generateRandomArray()
+    },
+    reMergeSort: function(){
+        this.sortedarr = mergeSort(Array.from(this.arr))
+    }
+  }     
 }
 </script>
 
