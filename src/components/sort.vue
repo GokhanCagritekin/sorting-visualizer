cc167f38a1afd9fb78fe3273fab77fe09d19bb31
Gökhan Çağrıtekin <gokhan.cagritekin@gmail.com>
2021-10-04 19:31:30 +0300

    change block width with button

--- src/components/sort.vue
+++ src/components/sort.vue
@@ -1,14 +1,20 @@
 <template>
 <div>
-  <div v-show="arr!=[]">
+    <div id="idbox" class="box">box</div>
+    <div id="changewidth">
+        <button v-on:click="changeWidth">Change Width</button>        
+    </div>
+    <br>
+  <div v-show="arr.length != 0">
     <h1>{{ arr }}</h1>
     </div>
-    <div v-show="sortedarr!=[]">
+    <div v-show="sortedarr.length != 0">
     <h1>{{ sortedarr }}</h1>
      </div>
     <div id="buttongen">
         <button v-on:click="regenerateRandomArray">Generate Array</button>        
     </div>
+    <br>
     <div id="buttonsort">
         <button v-on:click="reMergeSort">Sort Array</button>        
     </div> 
@@ -34,12 +40,23 @@ export default {
     },
     reMergeSort: function(){
         this.sortedarr = mergeSort(Array.from(this.arr))
+    },
+    changeWidth: function(){
+        document.getElementById('idbox').style.width = '100px'
     }
   }     
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+
+
+
+.box{
+    background-color: #2D9CDB;
+    border: 0 solid #27AE60;
+    width: 600px;
+}
 
 </style>
